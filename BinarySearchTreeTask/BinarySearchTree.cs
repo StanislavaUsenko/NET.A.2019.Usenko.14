85848e648bf092b1315a9dbdd2382e15cf406bd6
StanislavaUsenko <StanislavaUsenko@gmail.com>
2019-09-15 17:38:46 +0300

    added project(08.09.2019)
    added project today, but do this task 08.09.2019 (forgot to push it)

---
+++ BinarySearchTreeTask/BinarySearchTree.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeTask
+{
+    class BinarySearchTree<T> : IEnumerable<T>, IEnumerable
+    {    
+        private readonly Comparison<T> _compare;
+
+        private Node<T> _root;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public BinarySearchTree() : this((Comparison<T>)null) { }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="comparer"> Compares two instances</param>
+        public BinarySearchTree(IComparer<T> comparer) : this(comparer.Compare) { }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public BinarySearchTree(Comparison<T> comparison)
+        {
+            _root = null;
+            _compare = comparison ?? Comparer<T>.Default.Compare;
+        }
+
+        /// <summary>
+        /// Finds the element in the tree
+        /// </summary>
+        /// <param name="element"> Element to find </param>
+        /// <returns> True if the element exists </returns>
+        public bool Contains(T element) => Contains(_root, element);
+
+        /// <summary>
+        /// Clears the tree
+        /// </summary>
+        public void Clear()
+        {
+            _root = null;
+        }
+
+
+        /// <summary>
+        /// Adds an element in the tree
+        /// </summary>
+        /// <param name="element"> Element to add </param>
+        public void Add(T element)
+        {
+            if (element == null)
+                throw new ArgumentNullException(nameof(element));
+
+            _root = AddElement(_root, element);
+        }
+
+        /// <summary>
+        /// Adds a collection in the tree
+        /// </summary>
+        /// <param name="coll"> Collection to add </param>
+        public void Add(IEnumerable<T> coll)
+        {
+            if (coll == null)
+                throw new ArgumentNullException(nameof(coll));
+
+            foreach (var t in coll)
+            {
+                Add(t);
+            }
+        }
+
+
+        /// <summary>
+        /// Preorder traversal
+        /// </summary>
+        /// <returns> IEnumerable for preorder </returns>
+        public IEnumerable<T> PreOrder() => PreOrder(_root);
+
+        /// <summary>
+        /// Postorder traversal
+        /// </summary>
+        /// <returns> IEnumerable for postorder</returns>
+        public IEnumerable<T> PostOrder() => PostOrder(_root);
+
+        /// <summary>
+        /// Inorder traversal
+        /// </summary>
+        /// <returns> IEnumerable for inorder </returns>
+        public IEnumerable<T> InOrder() => InOrder(_root);
+        
+        /// <summary>
+        /// Iterator
+        /// </summary>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return InOrder().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+
+        private Node<T> AddElement(Node<T> node, T x)
+        {
+            if (node == null)
+            {
+                return new Node<T>(x);
+            }
+
+            int temp = _compare(x, node.Value);
+
+            if (temp < 0)
+                node.Left = AddElement(node.Left, x);
+            else if (temp > 0)
+                node.Right = AddElement(node.Right, x);
+
+            return node;
+        }
+
+        private bool Contains(Node<T> node, T element)
+        {
+            if (node == null)
+                return false;
+
+            int temp = _compare(node.Value, element);
+
+            if (temp == 0)
+                return true;
+            else if (temp < 0)
+                return Contains(node.Right, element);
+            else
+                return Contains(node.Left, element);
+        }
+
+        private IEnumerable<T> PreOrder(Node<T> node)
+        {
+            yield return node.Value;
+
+            if (node.Left != null)
+                foreach (var n in PreOrder(node.Left))
+                    yield return n;
+
+            if (node.Right != null)
+                foreach (var n in PreOrder(node.Right))
+                    yield return n;
+        }
+
+        private IEnumerable<T> InOrder(Node<T> node)
+        {
+            if (node.Left != null)
+                foreach (var n in InOrder(node.Left))
+                    yield return n;
+
+            yield return node.Value;
+
+            if (node.Right != null)
+                foreach (var n in InOrder(node.Right))
+                    yield return n;
+        }
+
+        private IEnumerable<T> PostOrder(Node<T> node)
+        {
+            if (node.Left != null)
+                foreach (var n in PostOrder(node.Left))
+                    yield return n;
+
+            if (node.Right != null)
+                foreach (var n in PostOrder(node.Right))
+                    yield return n;
+
+            yield return node.Value;
+        }
+        
+
+        internal class Node<T>
+        {
+            public Node<T> Left { get; set; }
+
+            public Node<T> Right { get; set; }
+
+            public T Value { get; set; }
+
+            public Node(T value)
+            {
+                Value = value;
+                Left = null;
+                Right = null;
+            }
+        }
+    }
+}
---
+++ QueryTask/Queue.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryTask
+{ 
+    public class Queue<T> : IEnumerable<T>, IEnumerable
+    {
+        private T[] _array;
+        private int size;
+        private int capacity;
+        private int head;
+        private int tail;
+
+        public Queue(int Size)
+        {
+            this.size = Size;
+            this._array = new T[Size];
+            this.head = -1;
+            this.tail = 0;
+        }
+
+        /// <summary>
+        /// Check is empty
+        /// </summary>
+        /// <returns></returns>
+        public bool isEmpty()
+        { 
+            return size == 0;
+        }
+
+        /// <summary>
+        /// Add element in Queue<T>
+        /// </summary>
+        /// <param name="newElement">Element to add</param>
+        public void Enqueue(T newElement)
+        {
+            if (this.size == this.capacity)
+            {
+                T[] newQueue = new T[2 * capacity];
+                Array.Copy(_array, 0, newQueue, 0, _array.Length);
+                _array = newQueue;
+                capacity *= 2;
+            }
+            size++;
+            _array[tail++ % capacity] = newElement;
+        }
+
+        /// <summary>
+        /// Delete element in Queue<T>
+        /// </summary>
+        public T Dequeue()
+        {
+            if (this.size == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            size--;
+            return _array[++head % capacity];
+        }
+
+        /// <summary>
+        /// Count of Queue<T>
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return this.size;
+            }
+        }
+
+        /// <summary>
+        /// Iterator
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new CustomIterator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// Class for custom iterator
+        /// </summary>
+        public class CustomIterator : IEnumerator<T>
+        {
+            private readonly Queue<T> container;
+            private int currentIndex;
+
+            /// <summary>
+            /// ctor
+            /// </summary>
+            public CustomIterator() { }
+
+            /// <summary>
+            /// ctor
+            /// </summary>
+            /// <param name="container"></param>
+            public CustomIterator(Queue<T> container)
+            {
+                currentIndex = container.head - 1;
+                this.container = container;
+            }
+
+            /// <summary>
+            /// Current element
+            /// </summary>
+            public T Current
+            {
+                get
+                {
+                    if (currentIndex < 0 || currentIndex > container.tail)
+                        throw new InvalidOperationException();
+                    return container._array[currentIndex];
+                }
+            }
+
+            /// <summary>
+            /// Method for return current element
+            /// </summary>
+            object IEnumerator.Current => Current;
+
+            public void Dispose()  {  }
+
+            /// <summary>
+            /// Сhecking for the existence of the next element
+            /// </summary>
+            /// <returns></returns>
+            public bool MoveNext()
+            {
+                currentIndex = (currentIndex + 1);
+
+                return currentIndex != container.tail;
+            }
+
+            /// <summary>
+            /// Counter reset
+            /// </summary>
+            public void Reset()
+            {
+                currentIndex = container.head - 1;
+            }
+        }
+    }
+
+    
+}
